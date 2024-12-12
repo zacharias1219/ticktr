be8b67bcfc8587a9e9965ef8fe7b6df50b563222
Richard Abishai <rickoshade1891@gmail.com>
2024-12-12 19:41:14 +0530

    Add Stripe integration and storage URL utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Id } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function useStorageUrl(storageId: Id<"_storage"> | undefined) {
+  return useQuery(api.storage.getUrl, storageId ? { storageId } : "skip");
 }
