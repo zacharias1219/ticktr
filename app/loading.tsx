965666b13326450beabd094c798e50bc4ca05c46
Richard Abishai <rickoshade1891@gmail.com>
2024-12-12 19:22:57 +0530

    Add Spinner component for loading indication

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
-// import Spinner from "@/components/Spinner";
+import Spinner from "@/components/Spinner";
 
 function Loading() {
-//   return <Spinner />;
+  return <Spinner />;
 }
 
 export default Loading;
